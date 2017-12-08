1c137d1c3cd477ff004d0f7360b0fcea9ed476c8
pereZZZ <perezzz@meta.ua>
2017-12-08 12:41:35 +0200

    vitya chekout

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const app = express();
 var bodyParser = require('body-parser');
-
+var jsonParser = bodyParser.json();
 var aaa = require('./routes/aaa');
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+var urlencodedParser = bodyParser.urlencoded({extended: true});
+var url = "mongodb://admin:admin@ds113906.mlab.com:13906/bd-db";
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -10,9 +14,25 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 var jsonParser = bodyParser.json();
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.post('/msgtobd', urlencodedParser, function(req,res){
+    var body = req.body
+})
+
+io.on('connection', (client) => {
+    console.log('client conekt')
+    client.on('msgtochat',(x)=>{
+        client.emit('msgfromchat',x);
+        client.broadcast.emit('msgfromchat',x)
+    });
+    client.on('disconnect', function(){
+        console.log('client disconect')
+    })
+});
+
+const port = 8000;
+io.listen(port);
+console.log('listening on port to socket', port);
 
 app.use('/aaa', aaa);
 
-
-app.listen(3001, () => console.log('Example app listening on port 3001!'))
+app.listen(3001, () => console.log('listening on port to 3001!'))
